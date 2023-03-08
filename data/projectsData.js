6271732c598e647c5e71950fb292d335aac15ecd
CosmicLatte009 <ohhm009@gmail.com>
2023-03-09 00:49:04 +0900

    docs: Projects 페이지 내용 수정

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,33 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '금방내방',
+    description: `다양한 사용자들의 방구석 인테리어 트렌드 및 일상을 공유할 수 있는 SNS서비스`,
+    imgSrc: '/static/images/soon-my-room-banner.png',
+    href: 'https://github.com/soon-my-room/soon_my_room',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'TODO 사이트',
+    description: `TypeScript와 React로 만든 토이 TODO`,
+    imgSrc: '/static/images/wanted-todo-banner.png',
+    href: 'https://github.com/CosmicLatte009/wanted-pre-onboarding-challenge-fe-1',
+  },
+  {
+    title: '크리스마스 MBTI 유형 테스트',
+    description: `연애할 때 내 질투의 정도를 알아보는 재미용 심리 테스트`,
+    imgSrc: '/static/images/christmas-mbti-banner.png',
+    href: 'https://github.com/CosmicLatte009/christmas_mbti_test',
+  },
+  {
+    title: '콜라콜라 벤딩머신',
+    description: `벤딩머신 토이 프로젝트`,
+    imgSrc: '/static/images/vending-machine-banner.png',
+    href: 'https://cosmiclatte009.github.io/Vending_machine_FE2/',
+  },
+  {
+    title: '정글 시네마',
+    description: `영화 목록 마크업`,
+    imgSrc: '/static/images/jungle-cinema-banner.png',
+    href: 'https://cosmiclatte009.github.io/Jungle_Cinema_FE2/',
   },
 ]
 
